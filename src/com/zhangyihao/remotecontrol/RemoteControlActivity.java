6817806534c90c300b69ed85bc9d74d9360e3cfd
zhangyihao <zyh5540@163.com>
2015-07-27 22:40:34 +0800

    commit

---
+++ src/com/zhangyihao/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,20 @@
+package com.zhangyihao.remotecontrol;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Window;
+
+public class RemoteControlActivity extends SingleFragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected Fragment createFragment() {
+		return new RemoteControlFragment();
+	}
+
+}
